79bdf983f822725f694621922b58b1217e98573c
Praffullitt Jain <praffullitt2000@gmail.com>
2019-03-03 00:02:00 +0530

    smallboard scores in heuristic

--- team63.py
+++ team63.py
@@ -13,11 +13,28 @@ class Team63():
 		#Weight Variables
 		self.cellWeight1 = 3
 		self.cellWeight2 = 9
-		self.cellWeight3 = 27
-		self.smallboardwon = 81
-		self.SBWeight1 = 243
-		self.SBWeight2 = 729
-		self.SBWeight3 = 2187
+		self.smallboardwon = 27
+		self.SBWeight1 = 81
+		self.SBWeight2 = 243
+		self.SBWeight3 = 729
+		
+		#Cell count variables
+		self.cellcount1x = 0
+		self.cellcount2x = 0 
+		self.cellcount3x = 0 
+		self.cellcount1o = 0
+		self.cellcount2o = 0 
+		self.cellcount3o = 0 
+
+		#SB count variables
+		self.SBcount1x = 0
+		self.SBcount2x = 0 
+		self.SBcount3x = 0 
+		self.SBcount1o = 0
+		self.SBcount2o = 0 
+		self.SBcount3o = 0
+		
+		self.sbs_score = 0
 
 
 	def move(self, board, old_move, flag):
@@ -80,16 +97,306 @@ class Team63():
 				
 			return best;
 
-	def heuristic(self, board):
-		winner = board.find_terminal_state();
-		if(winner[0] == 'x'):
-			return 10;
-		elif(winner[0] == 'o'):
-			return -10;
-		else:
-			return 0;
+	#calculating cellcount variables
+	def calculate(self, number_of_x, number_of_o):
+		if(number_of_x == 1 and number_of_o == 0):
+			self.cellcount1x ++;
+		elif(number_of_x == 2 and number_of_o == 0):
+			self.cellcount2x ++;
+		elif(number_of_o == 1 and number_of_x == 0):
+			self.cellcount1o ++;
+		elif(number_of_o == 2 and number_of_x == 0):
+			self.cellcount2o ++;
+		elif(number_of_o == 3 and number_of_x == 0):
+			self.cellcount3o ++;
+		elif(number_of_x == 3 and number_of_o == 0):
+			self.cellcount3x ++;
 
-		#calculating smallboard score
+	def reinitialize(self):
+		#re-initialize these values for next iteration of smallboards. 
+		self.cellcount1x = 0
+		self.cellcount2x = 0 
+		self.cellcount3x = 0 
+		self.cellcount1o = 0
+		self.cellcount2o = 0 
+		self.cellcount3o = 0
+
+	#check if smallboard has been won
+	def check_win(self):
+		
+		if(self.cellcount3x > 0):
+			self.reinitialize();
+			self.sbs_score += self.smallboardwon;
+			return 1
+		if(self.cellcount3o > 0):
+			self.reinitialize();
+			self.sbs_score -= self.smallboardwon;
+			return 1
+		
+		return 0
+
+
+	def heuristic(self, board):
+		# winner = board.find_terminal_state();
+		# if(winner[0] == 'x'):
+		# 	return 10;
+		# elif(winner[0] == 'o'):
+		# 	return -10;
+		# else:
+		# 	return 0;
+		self.sbs_score = 0;
+		#calculating smallboard score. We have the board object with us. Let's visit each smallboard and calculate its score
+		#There are 3 rows of small boards 
+		
+		#First row of small boards i.e cell rows(0-2)
+		index1 = 0;
+		for k in range(2): #Big boards 1 and 2
+			
+			index2 = 0; #Column number of small boards, gets incremented at the end of this loop. 
+			for l in range(3): # Going through 3 small boards in one big board
+				
+				#Rows of the smallboard
+				for i in range(index1, index1+2):
+					number_of_x = 0;
+					number_of_o = 0;
+					for j in range(index2, index2+2):
+						if ( board.big_boards_status[k][i][j] == 'x'):
+							number_of_x++;
+						if ( board.big_boards_status[k][i][j] == 'o'):
+							number_of_o++;
+					self.calculate(number_of_x, number_of_o)
+				
+				#checking if smallboard is won
+				ans = check_win();
+				if(ans == 1):
+					continue;
+				
+					
+				#Columns of the smallboard
+				for i in range(index1, index1+2):
+					number_of_x = 0;
+					number_of_o = 0;
+					for j in range(index2, index2+2):
+						if ( board.big_boards_status[k][j][i] == 'x'):
+							number_of_x++;
+						if ( board.big_boards_status[k][j][i] == 'o'):
+							number_of_o++;
+					self.calculate(number_of_x, number_of_o)
+
+				#checking if smallboard is won
+				ans = check_win();
+				if(ans == 1):
+					continue;
+				
+				#Major diagonal of the smallboard
+				number_of_x = 0;
+				number_of_o = 0;
+				for i in range(index1, index1+2):
+					if ( board.big_boards_status[k][i][i] == 'x'):
+						number_of_x++;
+					if ( board.big_boards_status[k][i][i] == 'o'):
+						number_of_o++;
+				self.calculate(number_of_x, number_of_o)
+				#checking if smallboard is won
+				ans = check_win();
+				if(ans == 1):
+					continue;
+
+				#for sb1, diagonal 2
+				number_of_x = 0;
+				number_of_o = 0;
+				for i in range(index1, index1+2):
+					if ( board.big_boards_status[k][i][2-i] == 'x'):
+						number_of_x++;
+					if ( board.big_boards_status[k][i][2-i] == 'o'):
+						number_of_o++;
+				self.calculate(number_of_x, number_of_o)
+				#checking if smallboard is won
+				ans = check_win();
+				if(ans == 1):
+					continue;
+
+				#applying our formula for calculating score for this smallboard
+				total_myscore = (self.cellWeight1 * self.cellcount1x * self.cellcount1x) + (self.cellWeight2 * self.cellcount2x * self.cellcount2x)
+				total_oppscore = (self.cellWeight1 * self.cellcount1o * self.cellcount1o) + (self.cellWeight2 * self.cellcount2o * self.cellcount2o)
+				total = total_myscore - total_oppscore;
+				self.sbs_score += total;
+
+				#re-initialize these values for next iteration of smallboards. 
+				self.cellcount1x = 0
+				self.cellcount2x = 0 
+				self.cellcount3x = 0 
+				self.cellcount1o = 0
+				self.cellcount2o = 0 
+				self.cellcount3o = 0
+				#Move on to next smallboard in that row of smallboards i.e cell columns += 3
+				index2 += 3;
+
+		#Second row of smallboards i.e cell rows(3-5)	
+		index1 = 3;
+		for k in range(2): #Big boards 1 and 2
+			
+			index2 = 0; #Column number of small boards, gets incremented at the end of this loop. 
+			for l in range(3): # Going through 3 small boards in one big board
+				
+				#Rows of the smallboard
+				for i in range(index1, index1+2):
+					number_of_x = 0;
+					number_of_o = 0;
+					for j in range(index2, index2+2):
+						if ( board.big_boards_status[k][i][j] == 'x'):
+							number_of_x++;
+						if ( board.big_boards_status[k][i][j] == 'o'):
+							number_of_o++;
+					self.calculate(number_of_x, number_of_o)
+				#checking if smallboard is won
+				ans = check_win();
+				if(ans == 1):
+					continue;
+					
+
+				#Columns of the smallboard
+				for i in range(index1, index1+2):
+					number_of_x = 0;
+					number_of_o = 0;
+					for j in range(index2, index2+2):
+						if ( board.big_boards_status[k][j][i] == 'x'):
+							number_of_x++;
+						if ( board.big_boards_status[k][j][i] == 'o'):
+							number_of_o++;
+					self.calculate(number_of_x, number_of_o)
+				#checking if smallboard is won
+				ans = check_win();
+				if(ans == 1):
+					continue;
+				
+				#Major diagonal of the smallboard
+				number_of_x = 0;
+				number_of_o = 0;
+				for i in range(index1, index1+2):
+					if ( board.big_boards_status[k][i][i] == 'x'):
+						number_of_x++;
+					if ( board.big_boards_status[k][i][i] == 'o'):
+						number_of_o++;
+				self.calculate(number_of_x, number_of_o)
+				#checking if smallboard is won
+				ans = check_win();
+				if(ans == 1):
+					continue;
+
+				#for sb1, diagonal 2
+				number_of_x = 0;
+				number_of_o = 0;
+				for i in range(index1, index1+2):
+					if ( board.big_boards_status[k][i][2-i] == 'x'):
+						number_of_x++;
+					if ( board.big_boards_status[k][i][2-i] == 'o'):
+						number_of_o++;
+				self.calculate(number_of_x, number_of_o)
+				#checking if smallboard is won
+				ans = check_win();
+				if(ans == 1):
+					continue;
+
+				#applying our formula for calculating score for this smallboard
+				total_myscore = (self.cellWeight1 * self.cellcount1x * self.cellcount1x) + (self.cellWeight2 * self.cellcount2x * self.cellcount2x)
+				total_oppscore = (self.cellWeight1 * self.cellcount1o * self.cellcount1o) + (self.cellWeight2 * self.cellcount2o * self.cellcount2o)
+				total = total_myscore - total_oppscore;
+				self.sbs_score += total;
+
+				#re-initialize these values for next iteration of smallboards. 
+				self.cellcount1x = 0
+				self.cellcount2x = 0 
+				self.cellcount3x = 0 
+				self.cellcount1o = 0
+				self.cellcount2o = 0 
+				self.cellcount3o = 0
+				#Move on to next smallboard in that row of smallboards i.e cell columns += 3
+				index2 += 3;
+
+		#Third row of smallboards i.e cell rows(6-8)
+		index1 = 6;
+		for k in range(2): #Big boards 1 and 2
+			
+			index2 = 0; #Column number of small boards, gets incremented at the end of this loop. 
+			for l in range(3): # Going through 3 small boards in one big board
+				
+				#Rows of the smallboard
+				for i in range(index1, index1+2):
+					number_of_x = 0;
+					number_of_o = 0;
+					for j in range(index2, index2+2):
+						if ( board.big_boards_status[k][i][j] == 'x'):
+							number_of_x++;
+						if ( board.big_boards_status[k][i][j] == 'o'):
+							number_of_o++;
+					self.calculate(number_of_x, number_of_o)
+				#checking if smallboard is won
+				ans = check_win();
+				if(ans == 1):
+					continue;
+					
+
+				#Columns of the smallboard
+				for i in range(index1, index1+2):
+					number_of_x = 0;
+					number_of_o = 0;
+					for j in range(index2, index2+2):
+						if ( board.big_boards_status[k][j][i] == 'x'):
+							number_of_x++;
+						if ( board.big_boards_status[k][j][i] == 'o'):
+							number_of_o++;
+					self.calculate(number_of_x, number_of_o)
+				#checking if smallboard is won
+				ans = check_win();
+				if(ans == 1):
+					continue;
+				
+				#Major diagonal of the smallboard
+				number_of_x = 0;
+				number_of_o = 0;
+				for i in range(index1, index1+2):
+					if ( board.big_boards_status[k][i][i] == 'x'):
+						number_of_x++;
+					if ( board.big_boards_status[k][i][i] == 'o'):
+						number_of_o++;
+				self.calculate(number_of_x, number_of_o)
+				#checking if smallboard is won
+				ans = check_win();
+				if(ans == 1):
+					continue;
+
+				#for sb1, diagonal 2
+				number_of_x = 0;
+				number_of_o = 0;
+				for i in range(index1, index1+2):
+					if ( board.big_boards_status[k][i][2-i] == 'x'):
+						number_of_x++;
+					if ( board.big_boards_status[k][i][2-i] == 'o'):
+						number_of_o++;
+				self.calculate(number_of_x, number_of_o)
+				#checking if smallboard is won
+				ans = check_win();
+				if(ans == 1):
+					continue;
+
+				#applying our formula for calculating score for this smallboard
+				total_myscore = (self.cellWeight1 * self.cellcount1x * self.cellcount1x) + (self.cellWeight2 * self.cellcount2x * self.cellcount2x)
+				total_oppscore = (self.cellWeight1 * self.cellcount1o * self.cellcount1o) + (self.cellWeight2 * self.cellcount2o * self.cellcount2o)
+				total = total_myscore - total_oppscore;
+				self.sbs_score += total;
+
+				#re-initialize these values for next iteration of smallboards. 
+				self.cellcount1x = 0
+				self.cellcount2x = 0 
+				self.cellcount3x = 0 
+				self.cellcount1o = 0
+				self.cellcount2o = 0 
+				self.cellcount3o = 0
+				#Move on to next smallboard in that row of smallboards i.e cell columns += 3
+				index2 += 3;
+
+		#Okay, now we have calculated score of each smallboard. Now let's calculate game status!
 
 
 	def isMovesLeft(self, board, cell):
